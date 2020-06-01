b9a7e8d0166532c1cd83d81ed26d3d83b2fdae47
Kiko Beats <josefrancisco.verdu@gmail.com>
2020-06-01 11:36:22 +0200

    refactor: style

--- index.js
+++ index.js
@@ -2,29 +2,14 @@
 
 const escapeStringRegexp = require('escape-string-regexp')
 
-const DEFAULTS = {
-  OPTS: {
-    replacement: '',
-    flags: ''
-  }
+module.exports = ({ replacement = '', flags = '' } = {}) => (input, regex) => {
+  const matches = input.match(regex)
+  const match = matches ? matches[0] : undefined
+  const pattern = match && escapeStringRegexp(match)
+  const replacer = RegExp(pattern, flags)
+
+  const output = input.replace(replacer, replacement)
+  const test = match !== undefined && match !== null
+
+  return { test, match, input, output }
 }
-
-function factory (opts) {
-  const { replacement, flags } = Object.assign({}, DEFAULTS.OPTS, opts)
-
-  function strmatch (input, regex) {
-    const matches = input.match(regex)
-    const match = matches ? matches[0] : undefined
-    const pattern = match && escapeStringRegexp(match)
-    const replacer = RegExp(pattern, flags)
-
-    const output = input.replace(replacer, replacement)
-    const test = match !== undefined && match !== null
-
-    return { test, match, input, output }
-  }
-
-  return strmatch
-}
-
-module.exports = factory
